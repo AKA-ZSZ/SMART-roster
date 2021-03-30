52e0e3e8a91a9b411e7677326c3366c97f046aac
Steven <zzt2143@hotmail.com>
2021-03-29 23:47:07 -0700

    updated the Pipfile

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 # mydir = os.getcwd()
 # mydir_tmp = mydir + "\\code"
-os.system('pip install -r requirements.txt')
 mydir_tmp = f"{os.getcwd()}/code"
 mydir_new = os.chdir(mydir_tmp) 
 os.system('python main.py')
