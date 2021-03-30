38fd1471e07e237f5837a280421ef563050dd97c
Steven <zzt2143@hotmail.com>
2021-03-29 23:25:06 -0700

    fix path #2

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 
 # mydir = os.getcwd()
 # mydir_tmp = mydir + "\\code"
-mydir_tmp = f"{os.path.dirname(os.getcwd())}/code"
+mydir_tmp = f"{os.getcwd()}/code"
 mydir_new = os.chdir(mydir_tmp) 
 os.system('python main.py')
