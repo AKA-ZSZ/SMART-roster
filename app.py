5276c02f0f72ea92e3c79587f480be619c39eec5
Steven <zzt2143@hotmail.com>
2021-03-29 23:29:50 -0700

    added a line to install requirements when running app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ import os
 # mydir_tmp = mydir + "\\code"
 mydir_tmp = f"{os.getcwd()}/code"
 mydir_new = os.chdir(mydir_tmp) 
+os.system('pip install -r requirements.txt')
 os.system('python main.py')
